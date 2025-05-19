d037702441bbd704c0810cae135b2b64d332f418
Highscoretech <highscoretechng@gmail.com>
2025-05-19 17:05:35 +0100

    mine

--- socket/index.js
+++ socket/index.js
@@ -2,6 +2,7 @@ const { Server } = require("socket.io");
 const Chat = require("../controllers/public_chat.controller.js");
 // const { CrashGameEngine } = require("../controllers/games/crash.controller.js");
 const { CrashGameEngine } = require('../controllers/games/crash');
+const { initMinesGame } = require('../controllers/games/mines');
 
 async function createsocket(httpServer) {
   const io = new Server(httpServer, {
@@ -23,6 +24,11 @@ crashGame.run((bet) => {
   // Handle bet callback if needed
   io.emit('new-bet', bet);
 });
+const minesGameEngine = initMinesGame(io);
+global.gameEngines = {
+  mines: minesGameEngine,
+  // other games...
+};
 
   io.on("connection", (socket) => {
     console.log("New client connected:", socket.id);
