48f0f9bf03dffb5d3bb4e355134cc14ed8b07cbb
Highscoretech <highscoretechng@gmail.com>
2025-05-19 06:08:45 +0100

    test 1

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(express.json({limit: '50mb'}));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true , limit: '50mb'}));
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://azebets.com","http://localhost:5173", "https://azebets.netlify.app", "https://azebet-admin.netlify.app" ]
+}));
 
 const server = createServer(app);
 async function main() {
